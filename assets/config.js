ca6eaa2c493a50ad44b6afa346f27cd53c2b6000
MAinulYaqin <ainul.y9b@gmail.com>
2018-05-07 14:36:17 +0700

    setting to production | :anchor:

--- assets/config.js
+++ assets/config.js
@@ -2,7 +2,7 @@ let env = process.env;
 let platform = env.platform;
 
 module.exports = {
-    node_env : env.NODE_ENV || 'development',
+    node_env : env.NODE_ENV || 'production', // Change production to development to use devTools
     open_devtools: platform === 'darwin' ? 'Command+Shift+I' : 'Ctrl+Shift+I',
     open_files: platform === 'darwin' ? 'Command+O' : 'Ctrl+O',
     reload: platform === 'darwin' ? 'Command+R' : 'Ctrl+R'
